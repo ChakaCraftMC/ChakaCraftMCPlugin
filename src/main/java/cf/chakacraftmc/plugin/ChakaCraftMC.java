184dbfb542b75073197cb35275029b3996ac167e
Chaka15205 <minecraftandmore86@gmail.com>
2016-01-01 14:07:40 -0500

    Started on player tracking, 1.0.0-B5

--- src/main/java/cf/chakacraftmc/plugin/ChakaCraftMC.java
+++ src/main/java/cf/chakacraftmc/plugin/ChakaCraftMC.java
@@ -2,6 +2,7 @@ package cf.chakacraftmc.plugin;
 
 import cf.chakacraftmc.plugin.command.CCMCCommand;
 import cf.chakacraftmc.plugin.lib.PermLib;
+import cf.chakacraftmc.plugin.listener.PlayerListener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -9,7 +10,7 @@ import java.util.logging.Level;
 
 public class ChakaCraftMC extends JavaPlugin {
 
-    public static final String VERSION = "1.0.0-B4";
+    public static final String VERSION = "1.0.0-B5";
 
     private PluginManager pm = this.getServer().getPluginManager();
 
@@ -24,6 +25,9 @@ public class ChakaCraftMC extends JavaPlugin {
 
         //Perm Lib Initialization
         PermLib.init(this.pm);
+
+        //Listeners
+        new PlayerListener(this);
 
         //Command Registration
         this.getCommand("ccmc").setExecutor(new CCMCCommand(this));
---
+++ src/main/java/cf/chakacraftmc/plugin/listener/PlayerListener.java
@@ -0,0 +1,52 @@
+package cf.chakacraftmc.plugin.listener;
+
+import cf.chakacraftmc.plugin.ChakaCraftMC;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.logging.Level;
+
+public class PlayerListener implements Listener {
+
+    private final ChakaCraftMC plugin;
+
+
+    public PlayerListener(ChakaCraftMC plugin) {
+        this.plugin = plugin;
+        this.plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+
+        Player player = event.getPlayer();
+
+        String name = player.getName();
+        String uuid = player.getUniqueId().toString();
+
+        try {
+            String driver = "org.gjt.mm.mysql.Driver";
+            String url = "jdbc:mysql://104.243.39.107/ccmc_pl";
+            Class.forName(driver);
+
+            Connection con = DriverManager.getConnection(url, "remote", "chris2112");
+
+            String query = "INSERT INTO players (username, uuid) VALUES (?, ?)";
+
+            PreparedStatement stmt = con.prepareStatement(query);
+            stmt.setString(1, name);
+            stmt.setString(2, uuid);
+
+            stmt.execute();
+
+            con.close();
+        } catch (Exception e) {
+            this.plugin.getLogger().log(Level.WARNING, "An Error has occurred: " + e);
+        }
+    }
+}
