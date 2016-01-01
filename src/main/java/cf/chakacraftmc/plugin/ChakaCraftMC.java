febc95a8136e88967a841c20ed19c62b9b1b35bd
Chaka15205 <minecraftandmore86@gmail.com>
2016-01-01 13:24:27 -0500

    You fucking idiot, 1.0.0-B4

--- src/main/java/cf/chakacraftmc/plugin/ChakaCraftMC.java
+++ src/main/java/cf/chakacraftmc/plugin/ChakaCraftMC.java
@@ -9,24 +9,24 @@ import java.util.logging.Level;
 
 public class ChakaCraftMC extends JavaPlugin {
 
-    public static final String VERSION = "1.0.0-B3";
+    public static final String VERSION = "1.0.0-B4";
 
     private PluginManager pm = this.getServer().getPluginManager();
 
     @Override
     public void onDisable() {
 
-        //Perm Lib Initialization
-        PermLib.init(this.pm);
-
-        //Command Registration
-        this.getCommand("ccmc").setExecutor(new CCMCCommand(this));
-
         this.getLogger().log(Level.INFO, "Disabled");
     }
 
     @Override
     public void onEnable() {
+
+        //Perm Lib Initialization
+        PermLib.init(this.pm);
+
+        //Command Registration
+        this.getCommand("ccmc").setExecutor(new CCMCCommand(this));
 
         this.getLogger().log(Level.INFO, "Enabled");
     }
